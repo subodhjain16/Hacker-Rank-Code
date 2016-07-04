e6d3d492ccc589a5c405705aad75403b2f8ab33a
subodhjain16 <er_jainsubodh@yahoo.com>
2016-07-04 14:50:19 +0530

    Add files via upload

---
+++ Contigious_Array/Contigious_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contigious_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            string[] arrbig = new string[num*2];
+            for (int i = 0; i < num * 2; i++)
+            {
+                arrbig[i] = Console.ReadLine();
+            }
+
+            for (int j = 0; j < num*2; j++)
+            {
+                int size = Convert.ToInt32(arrbig[j]);
+                string[] arr_temp = arrbig[j+1].Split(' ');
+                int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+                findsum(arr, size);
+                j++;
+            }
+            
+        }
+        22222222222222222222222221x ki
+    }
+}
